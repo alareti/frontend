bf1f51bee92b269b22c775e343ae5ee216fa2089
Davin Lampert <lampdavin@gmail.com>
2023-09-19 00:18:42 -0400

    Update page.tsx

--- src/app/world-building/page.tsx
+++ src/app/world-building/page.tsx
@@ -1,3 +1,91 @@
+import Link from "next/link";
+
 export default function Index() {
-  return <></>;
+  return (
+    <>
+      <h1>World Building</h1>
+      <p>
+        The main focus now is to build out the lore of the video game universe
+        called <i>Astral Realms</i>. Here is a summary of a lot of discussion
+        I&apos;ve had with ChatGPT to develop my thoughts on the core aspects of
+        the game. It is included below. Also found here is a table of contents
+        to explore more aspects of the game.
+      </p>
+      <br />
+      <br />
+      <h2>Table of Contents</h2>
+      <ul>
+        <li>
+          <Link href={"./"}>Qyano</Link>
+        </li>
+      </ul>
+      <br />
+      <br />
+      <h2>Echoes of the Astral Tide</h2>
+      <h3>Setting & Universe:</h3>
+      <p>
+        Set in an expansive astral universe influenced by a cosmic energy called
+        the &quot;Astral Tide,&quot; the game features diverse territories and
+        cultures impacted by a significant event known as &quot;The Great Astral
+        Shift.&quot; This shift, which altered the flow of the Astral Tide,
+        introduced alien species and various astral anomalies.
+      </p>
+      <h3>Characters:</h3>
+      <ul>
+        <li>
+          <strong>Qyano</strong>: A Tidebinder with a deep belief in
+          &quot;Astral Harmony.&quot; This space-age Don Quixote is profoundly
+          passionate about this concept, which shapes the universe around him.
+          Although players can customize parts of his story, his core commitment
+          to the Astral Harmony remains unyielding.
+        </li>
+        <li>
+          <strong>Pancho</strong>: Loyal to Qyano, this character acts as the
+          pragmatic Sancho Panza of the universe. He brings a grounding,
+          practical perspective, contrasting and balancing Qyano&apos;s
+          philosophical tendencies.
+        </li>
+      </ul>
+      <h3>Gameplay Dynamics:</h3>
+      <ol>
+        <li>
+          <strong>Combat</strong>: Turn-based, emphasizing strategy, with
+          Tidebinder abilities and environmental interactions.
+        </li>
+        <li>
+          <strong>Narrative Choices</strong>: Players navigate intricate
+          dialogues, influencing relationships, narrative progression, and
+          understanding of the Astral Harmony.
+        </li>
+        <li>
+          <strong>Influence System</strong>: Decisions impact affiliations with
+          factions and allies, leading to varied outcomes.
+        </li>
+      </ol>
+      <h3>Themes:</h3>
+      <p>
+        The game delves into themes of cosmic unity (Astral Harmony), the
+        complexities of friendship and loyalty (primarily between Qyano and
+        Pancho), the moral ambiguities of decisions, and the balance between
+        profound introspection and comedy. Additionally, players navigate a
+        galaxy rich in geopolitical intricacies, interacting with various
+        states, factions, and unique alien races.
+      </p>
+      <h3>Tonal Essence:</h3>
+      <p>
+        &quot;Echoes of the Astral Tide&quot; can be likened to a fusion of
+        &quot;Don Quixote&quot; and &quot;Star Wars.&quot; While it delves into
+        profound philosophical territories reminiscent of titles like KOTOR 2,
+        it also offers light-hearted, playful episodes, ensuring an engaging
+        experience throughout.
+      </p>
+      <p>
+        <em>
+          Overall, &quot;Echoes of the Astral Tide&quot; promises an immersive
+          RPG journey, skillfully blending classic literature influences with
+          cosmic exploration, intricate characters, and profound thematic depth.
+        </em>
+      </p>
+    </>
+  );
 }
