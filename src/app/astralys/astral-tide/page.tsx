f6b3ccddf6a6f7e362b782d92f050747b5562851
Davin Lampert <lampdavin@gmail.com>
2023-09-27 00:50:49 -0400

    Updated verbiage

--- src/app/astralys/astral-tide/page.tsx
+++ src/app/astralys/astral-tide/page.tsx
@@ -4,51 +4,56 @@ export default function Index() {
       <h1>Astral Tide</h1>
       <h2>Astral Tide Summary - Part 1: Overview</h2>
       <p>
-        The Astral Tide is an all-encompassing energy within a galaxy
-        reminiscent of the setting of Star Wars. Originating from ideas found in
-        various works like Arcanum, Star Wars, The Witcher, and Halo, the Tide
-        functions much like the Force in Star Wars. Sensitive users, called
-        Tidebinders, can harness its powers for supernatural feats, such as
-        telekinesis.
+        The <b>Astral Tide</b>, colloquially known as <b>tide</b>, is an
+        all-encompassing energy within a galaxy reminiscent of the setting of
+        Star Wars. Originating from ideas found in various works like Arcanum,
+        Star Wars, The Witcher, and Halo, the tide functions much like the Force
+        in Star Wars. Sensitive users, called <b>tidebinders</b>, can harness
+        its powers for supernatural feats, such as telekinesis.
       </p>
       <p>
-        In the galaxy&apos;s ancient times, only the Forerunners, a singular
-        sentient species, inhabited the stars. The Tide was then spread
-        homogenously, softly influencing reality. These Forerunners utilized a
-        mix of the Tide and technology, creating technotide artifacts that
-        allowed them to flourish.
+        In the galaxy&apos;s ancient times, only the <b>forerunners</b>, a
+        singular sentient species, inhabited the stars. The tide was then spread
+        homogenously, softly and symbiotically influencing reality. These
+        forerunners utilized a mix of the tide and conventional technology,
+        creating <b>forerunner artifacts</b> that allowed them to flourish.
       </p>
       <p>
-        However, stability was disrupted by The Great Astral Shift, introducing
-        various alien species to the galaxy. This event caused the Tide to
-        become erratic and heterogeneous, forming pockets of strong Tide
-        influence, areas devoid of it, and others that fluctuated.
+        However, stability was disrupted by the <b>Great Astral Shift</b> which
+        introduced various <b>alien species</b> including <b>humans</b> to the
+        galaxy. This event caused the tide to become erratic and heterogeneous,
+        forming areas like <b>high tide zones</b> (pockets of strong tide
+        influence), <b>low tide zones</b> (areas with weak tide influence),{" "}
+        <b>tide voids</b> (areas devoid of it), thus ripping out the foundations
+        upon which forerunner society was built upon.
       </p>
       <p>
-        In the aftermath, technology and the Tide were often in conflict. Places
-        with strong Tide presence caused technology to behave unpredictably.
-        Only skilled Tidebinders, through studying Forerunner artifacts, could
-        replicate the seamless integration once common. As for the Forerunners,
-        post-Shift struggles led them to near-extinction, leaving only a few
-        hidden throughout the galaxy.
+        In the wake of the Shift&apos;s aftermath,{" "}
+        <b>tide and technology are now often in conflict</b>. Places with strong
+        tide presence cause technology to behave unpredictably. Only skilled
+        tidebinders, through studying ancient forerunner artifacts, can emulate
+        the seamless integration once common, now known as{" "}
+        <b>technotide items</b>. As for the forerunners, post-Shift struggles
+        led them to near-extinction, leaving only a few hidden throughout the
+        galaxy.
       </p>
       <p>
-        The Tide is seen not merely as an energy but also as a will, directing
-        those aligned with its flow towards their destiny. However, those adept
-        can navigate and even influence its currents, changing their fate.
-        Resisting the Tide&apos;s direction, though, comes at considerable
-        physical, mental, and spiritual costs.
+        The tide is seen not merely as an <b>energy</b> but also as a{" "}
+        <b>will</b>, directing those aligned with its flow towards their
+        destiny. However, those adept can navigate and even influence its
+        currents, changing their fate. Resisting the tide&apos;s direction,
+        though, comes at considerable physical, mental, and spiritual costs.
       </p>
       <p>
-        Different cultures perceive the Tide through their lenses, ranging from
-        viewing it as a deity, a life force, a test, a blessing, or even as pure
-        chaos.
+        Different <b>cultures</b> perceive the tide through their lenses,
+        ranging from viewing it as a deity, a life force, a test, a blessing, or
+        even as pure chaos.
       </p>
       <h2>Astral Tide Summary - Part 2: Individual and Cultural Interaction</h2>
       <p>
-        The Tide deeply influences both the mental and physical spheres of
-        existence. Emotions both affect the Tide and are shaped by it. Powerful
-        Tidebinders can wield incredible abilities in areas with strong Tide
+        The tide deeply influences both the mental and physical spheres of
+        existence. Emotions both affect the tide and are shaped by it. Powerful
+        tidebinders can wield incredible abilities in areas with strong tide
         presence or even transport its power through Technotide Artifacts like
         Astral Batteries
       </p>
@@ -58,30 +63,26 @@ export default function Index() {
       </p>
       <ul>
         <li>
-          Tide Beacons: Devices to help navigate areas dense with the Tide.
+          Tide Beacons: Devices to help navigate areas dense with the tide.
         </li>
         <li>
-          Astral Lenses: Enhance a Tidebinder&apos;s vision, revealing hidden
+          Astral Lenses: Enhance a tidebinder&apos;s vision, revealing hidden
           patterns.
         </li>
         <li>
           Tide Stabilizers: Create pockets of stability in fluctuating areas.
         </li>
-        <li>
-          Tide Wells: Containers that store the Tide, potentially for use in
-          areas devoid of it.
-        </li>
       </ul>
-      <p>For Tide-insensitive individuals, the relationship varies:</p>
+      <p>For tide-insensitive individuals, the relationship varies:</p>
       <ul>
         <li>
-          Commoners might prefer Tide voids, relying on conventional tech.
+          Commoners might prefer tide voids, relying on conventional tech.
         </li>
         <li>
-          Adventurers could brave Tide-dense zones seeking fortune or danger.
+          Adventurers could brave tide-dense zones seeking fortune or danger.
         </li>
         <li>
-          Astral researchers, though insensitive, could be Tide-curious,
+          Astral researchers, though insensitive, could be tide-curious,
           studying its mysteries and artifacts.
         </li>
       </ul>
@@ -91,62 +92,62 @@ export default function Index() {
       </p>
       <ul>
         <li>
-          Tide Acceptors: Those who harmonize with the Tide, letting it guide
+          Tide Acceptors: Those who harmonize with the tide, letting it guide
           their actions.
         </li>
         <li>
-          Tide Navigators: Those who interact with and influence the Tide to
+          Tide Navigators: Those who interact with and influence the tide to
           impact their own destiny.
         </li>
         <li>
-          Tide Rebels: Those who resist the Tide&apos;s pull, whose force of
+          Tide Rebels: Those who resist the tide&apos;s pull, whose force of
           will defines their own destiny, but at a cost.
         </li>
       </ul>
-      <p>On a macro scale, the Tide varies across the galaxy:</p>
+      <p>On a macro scale, the tide varies across the galaxy:</p>
       <ul>
         <li>
-          High Tide Zones: Dense with Tide energy, they might exhibit floating
+          High Tide Zones: Dense with tide energy, they might exhibit floating
           landmasses, bioluminescent flora/fauna, and unpredictable
           technological behaviors.
         </li>
         <li>
-          Low Tide Zones: Almost void of Tide energy, they often rely on
+          Low Tide Zones: Almost void of tide energy, they often rely on
           traditional tech and could appear barren, with muted colors and little
           to no astral interference.
         </li>
         <li>
           Fluctuating Tide Zones: Unpredictable, transitioning between high and
-          low Tide states, leading to a mix of the above characteristics and
+          low tide states, leading to a mix of the above characteristics and
           often being treacherous to navigate.
         </li>
         <li>
-          Astral Borderlands: Transition zones between high and low Tide areas,
+          Astral Borderlands: Transition zones between high and low tide areas,
           hosting a blend of both worlds&apos; features, often acting as trade
           or cultural fusion points.
         </li>
       </ul>
       <p>
         Ancient Forerunner Zones would harken back to the pre-Shift era, where
-        tech and Tide coexisted seamlessly. These areas might have preserved
+        tech and tide coexisted seamlessly. These areas might have preserved
         cities, artifacts, and technotide devices. Though rare, they could act
         as bastions of knowledge and power for those who discover them.
       </p>
       <p>
-        The Astral Tide&apos;s pervasive nature shapes not only individuals and
+        The astral tide&apos;s pervasive nature shapes not only individuals and
         environments but also politics, economies, beliefs, legends, and more.
       </p>
       <h2>Astral Tide Summary - Part 3: Politics, Economy, and Expansion</h2>
       <p>
-        The Tide&apos;s omnipresence has inevitable consequences on the
+        The tide&apos;s omnipresence has inevitable consequences on the
         sociopolitical and economic landscape:
       </p>
       <ul>
         <li>
-          Trade Routes: Traders might favor Tide voids or stable zones, but the
+          Trade Routes: Traders might favor tide voids or stable zones, but the
           lure of forbidden technotide artifacts might tempt many to risk
-          fluctuating or high Tide areas, leading to specialized professions
-          like Tide-navigators or Tide-smugglers.
+          fluctuating or high tide areas, leading to specialized professions
+          like tide-navigators or tide-smugglers.
         </li>
         <li>
           Political Power: Regions with control over technotide artifacts or
@@ -155,23 +156,23 @@ export default function Index() {
           Forerunners.
         </li>
         <li>
-          Religious Sects: Given the mystique of the Tide, several religious or
-          cultist groups might emerge, worshipping the Tide, its anomalies, or
+          Religious Sects: Given the mystique of the tide, several religious or
+          cultist groups might emerge, worshipping the tide, its anomalies, or
           even the Forerunners themselves.
         </li>
         <li>
-          Military: Armies or mercenaries trained in Tide-dense areas, adept at
-          using or countering the unpredictable effects of the Tide, could be
-          highly sought after. Likewise, weaponry harnessing the Tide&apos;s
+          Military: Armies or mercenaries trained in tide-dense areas, adept at
+          using or countering the unpredictable effects of the tide, could be
+          highly sought after. Likewise, weaponry harnessing the tide&apos;s
           power could be the pinnacle of warfare technology.
         </li>
       </ul>
       <p>Expansion Ideas:</p>
       <ul>
         <li>
-          Tide-induced Evolution: Over time, species living in strong Tide zones
+          Tide-induced Evolution: Over time, species living in strong tide zones
           might evolve unique abilities or features. This can lead to a diverse
-          range of species, each adapted to their local Tide conditions.
+          range of species, each adapted to their local tide conditions.
         </li>
         <li>
           Lost Forerunner Expeditions: Exploratory quests to find lost
@@ -180,25 +181,25 @@ export default function Index() {
           promise.
         </li>
         <li>
-          Tide Festivals: In regions where the Tide is celebrated, annual events
-          might mark significant astral occurrences, with Tidebinders showcasing
+          Tide Festivals: In regions where the tide is celebrated, annual events
+          might mark significant astral occurrences, with tidebinders showcasing
           their skills, and traders exchanging rare artifacts.
         </li>
         <li>
-          Research Institutions: Think tanks dedicated to studying the Tide,
+          Research Institutions: Think tanks dedicated to studying the tide,
           aiming to replicate the Forerunner&apos;s harmony between tech and
-          Tide. These places could be centers of learning but also political
+          tide. These places could be centers of learning but also political
           intrigue.
         </li>
         <li>
           Astral Prophets: Individuals claiming to foresee the future by
-          interpreting the Tide&apos;s will. Their prophecies could guide or
+          interpreting the tide&apos;s will. Their prophecies could guide or
           mislead whole civilizations.
         </li>
       </ul>
       <p>
-        By integrating the Tide deeply into every facet of the galaxy&apos;s
-        life, from the mundane to the monumental, the Astral Tide universe is
+        By integrating the tide deeply into every facet of the galaxy&apos;s
+        life, from the mundane to the monumental, the Astral tide universe is
         teeming with stories, adventures, and mysteries waiting to unfold.
       </p>
     </>
--- src/app/astralys/page.tsx
+++ src/app/astralys/page.tsx
@@ -5,68 +5,70 @@ export default function Index() {
     <>
       <h1>Astralys</h1>
       <p>
-        The main focus here is to build out the lore of <i>Astralys</i>, a
-        distant galaxy awash with the cosmic might of the Astral Tide, once
-        seamlessly harnessed by the enigmatic Forerunners. But disaster struck,
-        and the Great Astral Shift ushered in tumultuous changes, toppling the
-        Forerunners&apos; reign and reshaping the galaxy&apos;s tapestry. New
-        species—from the fiery Pyronians to the enduring Dendrans—staked their
-        claims, each infused with distinct aspirations and cultural hues. This
-        shift spawned regions riddled with potent anomalies,
-        technologically-saturated voids, and mercurial astral zones. Amidst this
-        dynamic canvas, elusive Tidebinders wield the Astral Tide&apos;s
-        essence, myriad factions hustle to unravel the secrets of ancient
-        Technotide Artifacts, and sprawling empires grapple for dominion. Every
-        denizen of Astralys, whether ancient or newly introduced, wrestles with
-        their lineage and destiny in a galaxy brimming with enigma and tension.
+        The main focus here is to build out the lore of <i>Astralys</i>.{" "}
+        <i>Astralys</i> a fictional universe set in a distant galaxy awash with
+        the cosmic might of the Astral Tide, once seamlessly harnessed by the
+        enigmatic Forerunners. But disaster struck, and the Great Astral Shift
+        ushered in tumultuous changes, toppling the Forerunners&apos; reign and
+        reshaping the galaxy&apos;s tapestry. New species—from the fiery
+        Pyronians to the enduring Dendrans—staked their claims, each infused
+        with distinct aspirations and cultural hues. This shift spawned regions
+        riddled with potent anomalies, technologically-saturated voids, and
+        mercurial astral zones. Amidst this dynamic canvas, elusive Tidebinders
+        wield the Astral Tide&apos;s essence, myriad factions hustle to unravel
+        the secrets of ancient Technotide Artifacts, and sprawling empires
+        grapple for dominion. Every denizen of Astralys, whether ancient or
+        newly introduced, wrestles with their lineage and destiny in a galaxy
+        brimming with enigma and tension.
       </p>
       <p>
         <i>Astralys</i> is being crafted as a &quot;frame universe.&quot; This
         approach to world-building seeks to meld the boundless possibilities of
         a multiverse with the continuity and stability inherent in a singular
-        universe. When brought to fruition, a frame universe would permit a
-        broad spectrum of settings and themes to seamlessly interconnect and
+        universe. When brought to fruition, a frame universe permits a broad
+        spectrum of settings and themes to seamlessly interconnect and
         simultaneously unify the universe&apos;s historical narrative. This is a
         universe where vastly diverse stories converge to enrich a broader
-        tapestry while feeling intricately linked and logically consistent. It
-        is a universe where versatility aligns with significance, structural
-        logic bolsters creative freedom, and familiarity sparks intrigue. While
-        the concept is ambitious, the potential rewards are boundless.
+        tapestry while simultaneously feeling intricately linked and logically
+        consistent. It is a universe where versatility aligns with significance,
+        structural logic bolsters creative freedom, and familiarity sparks
+        intrigue. While the concept is ambitious, the potential rewards are
+        boundless.
       </p>
       <h2>Table of Contents</h2>
       <ul>
         <li>
-          <Link href={"/world-building/overviews"}>Overviews</Link>
+          <Link href={"/astralys/overviews"}>Overviews</Link>
         </li>
         <li>
-          <Link href={"/world-building/frame-universe"}>Frame Universe</Link>
+          <Link href={"/astralys/frame-universe"}>Frame Universe</Link>
         </li>
         <li>
-          <Link href={"/world-building/astral-tide"}>Astral Tide</Link>
+          <Link href={"/astralys/astral-tide"}>Astral Tide</Link>
         </li>
         <li>
-          <Link href={"/world-building/symphonae"}>The Symphonae</Link>
+          <Link href={"/astralys/symphonae"}>The Symphonae</Link>
         </li>
         <li>
-          <Link href={"/world-building/dendrans"}>The Dendrans</Link>
+          <Link href={"/astralys/dendrans"}>The Dendrans</Link>
         </li>
         <li>
-          <Link href={"/world-building/aquarids"}>The Aquarids</Link>
+          <Link href={"/astralys/aquarids"}>The Aquarids</Link>
         </li>
         <li>
-          <Link href={"/world-building/pyronians"}>The Pyronians</Link>
+          <Link href={"/astralys/pyronians"}>The Pyronians</Link>
         </li>
         <li>
-          <Link href={"/world-building/moivari"}>The Moivari</Link>
+          <Link href={"/astralys/moivari"}>The Moivari</Link>
         </li>
         <li>
-          <Link href={"/world-building/choreans"}>The Choreans</Link>
+          <Link href={"/astralys/choreans"}>The Choreans</Link>
         </li>
         <li>
-          <Link href={"/world-building/virelians"}>The Virelians</Link>
+          <Link href={"/astralys/virelians"}>The Virelians</Link>
         </li>
         <li>
-          <Link href={"/world-building/galactic-unity-assembly"}>
+          <Link href={"/astralys/galactic-unity-assembly"}>
             The Galactic Unity Assembly
           </Link>
         </li>
