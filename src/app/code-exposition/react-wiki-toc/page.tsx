5572d677f3e4550f38874319213f93e014eace1e
Davin Lampert <lampdavin@gmail.com>
2023-10-09 11:49:40 -0400

    Switch to Flat Tree

--- src/app/code-exposition/react-wiki-toc/page.tsx
+++ src/app/code-exposition/react-wiki-toc/page.tsx
@@ -1,18 +1,38 @@
 "use client";
 
-import { ReactNode, useCallback, useState } from "react";
+import { ReactNode, useCallback } from "react";
+import useIntegral from "@/src/utils/useIntegral";
 
-type NestedString = string | NestedString[];
-
-function Root({ children }: { children: ReactNode }) {
-  const [tree, setTree] = useState<NestedString[]>([]);
-
-  const addNode = useCallback((id: string) => {}, []);
-
-  return <></>;
+interface FlatTreeNode {
+  data: string;
+  location: number[];
 }
 
-function Node({ children, id }: { children: ReactNode; id: string }) {}
+function Root({ children }: { children: ReactNode }) {
+  const integrateNode = useCallback(
+    (integral: FlatTreeNode[], delta: FlatTreeNode) => {
+      return [...integral, delta];
+    },
+    [],
+  );
+
+  const [flatTree, addNode] = useIntegral<FlatTreeNode[], FlatTreeNode>(
+    [],
+    integrateNode,
+  );
+
+  return <>{}</>;
+}
+
+function Node({
+  children,
+  id,
+  location,
+}: {
+  children: ReactNode;
+  id: string;
+  location: number[];
+}) {}
 
 export default function Index() {
   return <></>;
