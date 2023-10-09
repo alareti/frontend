9458732d9e4d20457bfe6f65e79c1f573d5d6700
Davin Lampert <lampdavin@gmail.com>
2023-10-09 16:43:28 -0400

    Resetting

--- src/app/code-exposition/react-wiki-toc/page.tsx
+++ src/app/code-exposition/react-wiki-toc/page.tsx
@@ -1,55 +1,12 @@
 "use client";
 
-import {
-  ReactNode,
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-} from "react";
-import useIntegral from "@/src/utils/useIntegral";
-
-interface FlatTreeNode {
-  data: string;
-  location: number[];
-}
-
-const LocationContext = createContext<number[]>([]);
-const AddNodeContext = createContext<(delta: FlatTreeNode) => void>(
-  (delta: FlatTreeNode) => {},
-);
+import { ReactNode } from "react";
 
 function Root({ children }: { children: ReactNode }) {
-  const integrateNode = useCallback(
-    (integral: FlatTreeNode[], delta: FlatTreeNode) => {
-      console.log("Integrating");
-      return [...integral, delta];
-    },
-    [],
-  );
-
-  const [flatTree, addNode] = useIntegral<FlatTreeNode[], FlatTreeNode>(
-    [],
-    integrateNode,
-  );
-
-  return (
-    <AddNodeContext.Provider value={addNode}>
-      {children}
-    </AddNodeContext.Provider>
-  );
+  return <>{children}</>;
 }
 
 function Node({ children, id }: { children: ReactNode; id: string }) {
-  const addNode = useContext(AddNodeContext);
-
-  // useEffect(() => {
-  //   addNode({
-  //     data: id,
-  //     location: [],
-  //   });
-  // }, [addNode, id]);
-
   return <>{children}</>;
 }
 
